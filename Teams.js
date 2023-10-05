f5f0d337ef8e577d18a252b8eb312dd0c3f69495
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-10-05 03:52:05 +0300

    Add files via upload

---
+++ Teams.js
@@ -0,0 +1,173 @@
+let teams = [
+    {
+        team:"إي سي ميلان",
+        img:"./Teams/Ac milan.jpg"
+    },
+    {
+        team:"اياكس",
+        img:"./Teams/Ajax.jpg"
+    },
+    {
+        team:"أرسنال",
+        img:"./Teams/Arsenal.jpg"
+    },
+    {
+        team:"أستون فيلا",
+        img:"./Teams/Aston villa.jpg"
+    },
+    {
+        team:"أتلتيكو مدريد",
+        img:"./Teams/Atletico.jpg"
+    },
+    {
+        team:"برشلونه",
+        img:"./Teams/Barcelona.jpg"
+    },
+    {
+        team:"بايرن ميونخ",
+        img:"./Teams/Bayern.jpg"
+    },
+    {
+        team:"بنفيكا",
+        img:"./Teams/Benfica.jpg"
+    },
+    {
+        team:"برايتون",
+        img:"./Teams/Brighton.jpg"
+    },
+    {
+        team:"سيلتك الاسكتلندي",
+        img:"./Teams/Celtic.jpg"
+    },
+    {
+        team:"تشيلسي",
+        img:"./Teams/Chelsea.jpg"
+    },
+    {
+        team:"كوبنهاجن الدنماركي",
+        img:"./Teams/Copenhagen.jpg"
+    },
+    {
+        team:"بوروسيا دورتموند",
+        img:"./Teams/Dortmund.jpg"
+    },
+    {
+        team:"إنتر ميلان",
+        img:"./Teams/Inter milan.jpg"
+    },
+    {
+        team:"يوفنتوس",
+        img:"./Teams/Juventus.jpg"
+    },
+    {
+        team:"لانس الفرنسي",
+        img:"./Teams/Lens.jpg"
+    },
+    {
+        team:"ليفربول",
+        img:"./Teams/Liverpool.jpg"
+    },
+    {
+        team:"ليون",
+        img:"./Teams/Lyon.jpg"
+    },
+    {
+        team:"مانشستر سيتي",
+        img:"./Teams/Man City .jpg"
+    },
+    {
+        team:"مانشستر يونايتد",
+        img:"./Teams/Man united.jpg"
+    },
+    {
+        team:"مارسيليا",
+        img:"./Teams/Marseille.jpg"
+    },
+]
+
+let nTeams = [
+    {
+        nTeam:"الأرجنتين",
+        nImg:"./nTeams/Argentina.jpg"
+    },
+    {
+        nTeam:"بلجيكا",
+        nImg:"./nTeams/Belgium.jpg"
+    },
+    {
+        nTeam:"البرازيل",
+        nImg:"./nTeams/Brazil.jpg"
+    },
+    {
+        nTeam:"كرواتيا",
+        nImg:"./nTeams/Croatia.jpg"
+    },
+    {
+        nTeam:"التشيك",
+        nImg:"./nTeams/Czechia.jpg"
+    },
+    {
+        nTeam:"الدنمارك",
+        nImg:"./nTeams/Denmark.jpg"
+    },
+    {
+        nTeam:"انجلترا",
+        nImg:"./nTeams/England.jpg"
+    },
+    {
+        nTeam:"فرنسا",
+        nImg:"./nTeams/France.jpg"
+    },
+    {
+        nTeam:"المانيا",
+        nImg:"./nTeams/Germany.jpg"
+    },
+    {
+        nTeam:"اليونان",
+        nImg:"./nTeams/Greece.jpg"
+    },
+    {
+        nTeam:"إيطاليا",
+        nImg:"./nTeams/Italy.jpg"
+    },
+    {
+        nTeam:"هولندا",
+        nImg:"./nTeams/Netherland.jpg"
+    },
+    {
+        nTeam:"النرويج",
+        nImg:"./nTeams/Norway.jpg"
+    },
+    {
+        nTeam:"بولندا",
+        nImg:"./nTeams/Poland.jpg"
+    },
+    {
+        nTeam:"البرتغال",
+        nImg:"./nTeams/Portugal.jpg"
+    },
+    {
+        nTeam:"صربيا",
+        nImg:"./nTeams/Serbia.jpg"
+    },
+    {
+        nTeam:"إسبانيا",
+        nImg:"./nTeams/Spain.jpg"
+    },
+    {
+        nTeam:"السويد",
+        nImg:"./nTeams/Sweden.jpg"
+    },
+    {
+        nTeam:"سويسرا",
+        nImg:"./nTeams/Switzerland.jpg"
+    },
+    {
+        nTeam:"تركيا",
+        nImg:"./nTeams/Turkiye.jpg"
+    },
+    {
+        nTeam:"أوكرانيا",
+        nImg:"./nTeams/Ukraine.jpg"
+    },
+]
